7e404e166d78cd42cbb4619c111f851b6b4d3f03
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-05-01 13:42:27 -0700

    Initial Commit

---
+++ RandomCircles/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomCircles
+//
+//  Created by Vadiraj Hippargi on 5/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomCircles/RandomCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomCirclesApp.swift
+//  RandomCircles
+//
+//  Created by Vadiraj Hippargi on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct RandomCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
