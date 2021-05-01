f2e5f823d1743fc088a7e454a9f618cf3d1eb7d1
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-05-01 14:23:05 -0700

    Good Thinking

--- RandomCircles/ContentView.swift
+++ RandomCircles/ContentView.swift
@@ -7,10 +7,28 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    var r:Double = Double.random(in: 1..<250)/255
+    var g:Double = 150/255
+    var b:Double = 100/255
+    var o = 7
+    var w = Int.random(in: 100..<200)
+    
+    var xoff = Int.random(in: 1..<20)
+    var yoff = Int.random(in: 1..<20)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            ForEach(0 ..< 12) { item in
+                Rectangle()
+                    .foregroundColor(Color( red: Double.random(in: 1..<250)/255, green: Double.random(in: 1..<250)/255, blue: Double.random(in: 1..<250)/255, opacity:0.7))
+                    .frame(width: CGFloat(w), height: CGFloat(w))
+                    .offset(x: CGFloat(Int.random(in: -210..<200)), y: CGFloat(Int.random(in: -200..<300)))
+            }
+        }
     }
 }
 
